f29d627769d4c3fd48d36ae399a647b4ae266d83
vulecoff <lev@union.edu>
2024-10-16 10:38:09 -0400

    make func dynamically helper

--- sync_to_docker.ps1
+++ sync_to_docker.ps1
@@ -1,6 +1,6 @@
 $targetContainer = "mettalog"
 $hostFolder = "./synthcl/."
-$containerFolder = "/home/user/synthcl"
+$containerFolder = "/home/user/devspace/synthcl"
 
 if (-not $(docker ps --filter "name=$targetContainer" --filter "status=running" -q)) {
     echo "Container $targetContainer has not been started."
